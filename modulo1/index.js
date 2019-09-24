7e3b796d40d3c1fe3389fbf1ac0df4950d4d2bd9
Eduardo Uliana <edu02_rko@hotmail.com>
2019-09-23 22:20:09 -0300

    Adicionado middlewares

--- modulo1/index.js
+++ modulo1/index.js
@@ -5,21 +5,51 @@ const server = express();
 // Para aceitar body em formato json
 server.use(express.json());
 
+//Middleware de log
+server.use((req,res, next) => {
+    console.log(`Método: ${req.method}; URL: ${req.url}`);
+
+    //para continuar
+    return next();
+});
+
+//Middleware para verificiar se foi passado um nome de usuário
+function checkUserExists(req, res, next) {
+    if (!req.body.name) {
+        return res.status(400).json({ error: 'User not found on request body'});
+    }
+
+    return next();
+}
+
+//Middleware para verificiar se foi passado um index de usuário
+function checkUserInArray(req, res, next) {
+    const user = users[req.params.index];
+
+    if (!user) {
+        return res.status(400).json({ error: 'User does not exists'});
+    }
+
+    //Adicionando uma nova variável na requisição que pode ser usada abaixo
+    req.user = user;
+
+    return next();
+}
+
 const users = ['Eduardo', 'Luiz', 'Victor']
 
 // Query params - Buscar todos os usuários
-server.get('/users', (req, res) => {
+server.get('/users', checkUserInArray, (req, res) => {
     return res.json(users);
 });
 
 // Route params - Buscar usuário pelo index
-server.get('/users/:index', (req, res) => {
-    const { index } = req.params;
-    return res.json(users[index]);
+server.get('/users/:index', checkUserInArray, (req, res) => {
+    return res.json(req.user);
 });
 
 
-server.post('/users', (req, res) => {
+server.post('/users', checkUserExists, (req, res) => {
     const { name } = req.body;
 
     users.push(name);
@@ -27,7 +57,7 @@ server.post('/users', (req, res) => {
     return res.json(users);
 });
 
-server.put('/users/:index', (req,res) => {
+server.put('/users/:index', checkUserExists, checkUserInArray, (req,res) => {
     const { index } = req.params;
     const { name } = req.body;
 
@@ -36,7 +66,7 @@ server.put('/users/:index', (req,res) => {
     return res.json(users);
 });
 
-server.delete('/users/:index', (req,res) => {
+server.delete('/users/:index', checkUserInArray, (req,res) => {
     const { index } = req.params;
 
     //Percorre o vetor até a posição informada e apaga o número informado de itens
